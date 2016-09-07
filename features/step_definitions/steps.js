de394360f346d1f49667ac6684a5a476a1a0288f
bradymat <apprentice@StMac0-17.local>
2016-09-08 11:51:08 +1200

    yup

---
+++ features/step_definitions/steps.js
@@ -0,0 +1,5 @@
+var assert = require('cucumber-assert')
+var Url = require('url')
+var extend = require('xtend')
+
+var config = require('../../config')
---
+++ features/support/hooks.js
@@ -0,0 +1,19 @@
+var config = require('../../config')
+var app = require('../../app')
+var server
+
+module.exports = function () {
+
+  this.registerHandler('BeforeFeatures', (features, callback) => {
+    console.log('server starting....')
+    server = app.listen(config.proxy.port, () => {
+      callback()
+    })
+  })
+
+  this.registerHandler('AfterFeatures', (features, callback) => {
+    console.log('server stopping....')
+    server.close()
+    callback()
+  })
+}
