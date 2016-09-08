741f43962f769c7ab8da9189ac0e8dfef96317ba
bradymat <apprentice@StMac0-17.local>
2016-09-08 13:40:35 +1200

    time for jock to create Database with JSON

---
+++ features/music-index.feature
@@ -0,0 +1,6 @@
+Feature: List songs
+
+Scenario: I want to see the top 10 songs
+  Given I am on the page '/'
+  Then I can see the song "Life On Mars?"
+  And I can see the song "Let It Be"
--- features/step_definitions/steps.js
+++ features/step_definitions/steps.js
@@ -3,3 +3,16 @@ var Url = require('url')
 var extend = require('xtend')
 
 var config = require('../../config')
+
+module.exports = function () {
+
+  this.Given('I am on the page "$string"', function (pathname) {
+    browser.url(`http://localhost:5050${pathname}`)
+  })
+
+  this.Then('I can see the song "$string"', function (song, callback) {
+    var SongExists = browser.waitForExist(`td=${song}`)
+    assert.equal(SongExists, true, callback)
+  })
+
+}
