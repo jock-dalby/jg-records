724adbbef41b436b681569607d1e9da6f64a3d60
bradymat <apprentice@StMac0-17.local>
2016-09-08 16:28:12 +1200

    deploying to Heroku

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # jg-records
 Specialising in selling kick-ass records.
+
+Stats from http://www.telegraph.co.uk/culture/music/11621427/best-songs-of-all-time.html
--- app.js
+++ app.js
@@ -1,13 +1,27 @@
 var express = require('express')
+var exphbs = require('express-handlebars')
+var path = require('path')
+var bodyParser = require('body-parser')
+
 var app = express()
-var port = process.env.PORT || 3000
+
+// setup BODY PARSER
+app.use(bodyParser.urlencoded({extended: false}))
+
+// view engine setup
+app.engine('handlebars', exphbs({defaultLayout: 'main'}))
+app.set('view engine', 'handlebars')
+
+// serve the files in /public
+app.use(express.static(path.join(__dirname, 'public')))
+
+
+
+//---------------------ROUTES---------------------------
 
 app.get('/', function(req, res) {
-  res.send('Hello World!')
+  res.send('deployed to Heroku')
 })
 
-app.listen(port, function() {
-  console.log("We are up and running on port " + port)
-})
 
 module.exports = app
