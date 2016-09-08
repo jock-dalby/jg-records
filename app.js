d834788e764bb0be939d1e18b8b3816cdd6badbb
bradymat <apprentice@StMac0-17.local>
2016-09-08 17:19:55 +1200

    top 10 songs added

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express')
 var exphbs = require('express-handlebars')
 var path = require('path')
 var bodyParser = require('body-parser')
+var db = require('./db/db')
 
 var app = express()
 
@@ -21,7 +22,12 @@ app.use(express.static(path.join(__dirname, 'public')))
 //---------------------ROUTES---------------------------
 
 app.get('/', function(req, res) {
-  res.send('deployed to Heroku')
+  db.getAllSongs('songs', function (err, songs) {
+    if (err) { res.send('Songs not available')
+  } else {
+  res.render('songsIndex', songs)
+}
+})
 })
 
 
